6a00592046c859bd31d381c3a919d9777845b49b
harrisonmacey <95727486+harrisonmacey@users.noreply.github.com>
2023-11-19 10:43:36 -0800

    Removed react stuff, added backend and helper functions for api

---
+++ Backend/s3Helper.js
@@ -0,0 +1,9 @@
+// backend/s3Helper.js
+
+// Mock function to simulate S3 file upload
+async function uploadToS3(file) {
+    // Return a mock URL string for testing
+    return "https://mock-s3-url.com/your-uploaded-image.jpg";
+}
+
+module.exports = uploadToS3;
--- Frontend/javascript/imageupload.js
+++ Frontend/javascript/imageupload.js
@@ -2,21 +2,48 @@
 const fileInput = document.getElementById('fileInput');
 const uploadedImage = document.getElementById('uploadedImage');
 
+// Function to upload the image to the backend
+function uploadImageToBackend(file) {
+    // Create a FormData object and append the file
+    const formData = new FormData();
+    formData.append('file', file);
+
+    // Make a POST request to the backend with the file
+    fetch('http://localhost:3001/upload', {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Success:', data);
+        // Handle success - e.g., displaying a success message or updating the UI
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+        // Handle errors here, such as displaying an error message
+    });
+}
+
 // Add an event listener to the file input to handle file selection
 fileInput.addEventListener('change', (event) => {
-  const selectedFile = event.target.files[0];
+  event.preventDefault();
+    const selectedFile = event.target.files[0];
+    
 
-  if (selectedFile) {
-    // Create a FileReader to read the selected file
-    const reader = new FileReader();
+    if (selectedFile) {
+        // Create a FileReader to read the selected file
+        const reader = new FileReader();
 
-    // Add an event listener to the FileReader to handle file reading
-    reader.addEventListener('load', (event) => {
-      const imageUrl = event.target.result;
-      uploadedImage.src = imageUrl; // Display the uploaded image
-    });
+        // Add an event listener to the FileReader to handle file reading
+        reader.addEventListener('load', (event) => {
+            const imageUrl = event.target.result;
+            uploadedImage.src = imageUrl; // Display the uploaded image
+        });
 
-    // Read the selected file as a data URL (for images)
-    reader.readAsDataURL(selectedFile);
-  }
+        // Read the selected file as a data URL (for images)
+        reader.readAsDataURL(selectedFile);
+
+        // Upload the image to the backend
+        uploadImageToBackend(selectedFile);
+    }
 });
