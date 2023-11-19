808e43bdc7a238495562b8a5453392d8779cc873
harrisonmacey <95727486+harrisonmacey@users.noreply.github.com>
2023-11-19 12:29:35 -0800

    added api call

--- Frontend/javascript/imageupload.js
+++ Frontend/javascript/imageupload.js
@@ -2,17 +2,47 @@
 const fileInput = document.getElementById('fileInput');
 const uploadedImage = document.getElementById('uploadedImage');
 
+
 // Helper Function to Call Google Vision API
 function analyzeImageWithGoogleVision(imageData) {
-    // Implement the API call here
-    // Note: This will expose your API key in the frontend
+  const apiKey = 'AIzaSyCxpTRpoep0JDpdck5CScU38H7H9XF7Qd8';
+  const apiUrl = `https://vision.googleapis.com/v1/images:annotate?key=${apiKey}`;
+
+  const requestBody = {
+      requests: [
+          {
+              image: {
+                  content: imageData.split(',')[1] // Assuming imageData is a base64 string with a header
+              },
+              features: [
+                  { type: 'LABEL_DETECTION', maxResults: 10 }
+              ],
+          },
+      ],
+  };
+
+  fetch(apiUrl, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(requestBody),
+  })
+  .then(response => response.json())
+  .then(data => {
+      console.log('Google Vision API Response:', data);
+      // Process the response data as needed
+  })
+  .catch(error => {
+      console.error('Error calling Google Vision API:', error);
+  });
 }
+
 
 // Helper Function to Upload to S3
 function uploadToS3(imageData) {
     // Implement the S3 upload here
     // Note: This can expose your S3 credentials
 }
+
 
 // Function to process the image
 function processImage(file) {
