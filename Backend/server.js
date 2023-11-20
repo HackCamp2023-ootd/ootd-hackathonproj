bf01051dfccee5a30f997ff0d2e648f1ff20a7ee
harrisonmacey <95727486+harrisonmacey@users.noreply.github.com>
2023-11-19 16:20:06 -0800

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,46 +1,33 @@
 const express = require('express');
 const multer = require('multer');
-const cors = require('cors');
 const dotenv = require('dotenv');
-var MongoClient = require('mongodb').MongoClient;
+const cors = require('cors'); // Require CORS
+const fs = require('fs-extra');
+require('dotenv').config({ path: './backend/credentials.env' });
 
-dotenv.config({ path: './backend/credentials.env' });
-const app = express();
-app.use(express.json());
-app.use(cors());
+
+// Importing mock helper functions
+
+dotenv.config();
 const upload = multer({ dest: 'uploads/' });
+const app = express();
+app.use(cors());
 const port = 3001;
 
-var uri = "mongodb://harrisonmacey:<password>@ac-exkq8sr-shard-00-00.qykp6gp.mongodb.net:27017,ac-exkq8sr-shard-00-01.qykp6gp.mongodb.net:27017,ac-exkq8sr-shard-00-02.qykp6gp.mongodb.net:27017/?ssl=true&replicaSet=atlas-13x7sp-shard-0&authSource=admin&retryWrites=true&w=majority";
-
-
-
-// Endpoint to receive and save metadata
-app.post('/upload-metadata', async (req, res) => {
+// Endpoint to handle file uploads
+app.post('/upload', upload.single('file'), async (req, res) => {
     try {
-        const { tag, link, uniqueId } = req.body;
-        console.log("workgin2");
-
-        MongoClient.connect(uri, async function(err, client) {
-            console.log("connected successfully");
-            const collection = client.db("test").collection("devices");
-            
-            const doc = {
-                title: "Record of a Shriveled Datum",
-                content: "No bytes, no problem. Just insert a document, in MongoDB",
-              }
-              // Insert the defined document into the "haiku" collection
-              const result = await balls.insertOne(doc);
-
-            client.close();
-          });
-        
-        res.status(200).json({ message: 'Metadata saved successfully' });
+    
     } catch (error) {
-        console.error('Error saving metadata:', error);
-        res.status(500).send('Error saving metadata');
+        console.error('Error:', error);
+        res.status(500).send('Error processing file');
     }
 });
+
+app.post('/startPipeline', (req, res) => {
+    makeApiCall();
+    res.send('API call initiated');
+  });
 
 // Start the server
 app.listen(port, () => {
