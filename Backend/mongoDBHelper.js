8f9175a6987ea0d57c9db56403d2ca9d85f58926
Carlos <=>
2023-12-03 17:23:56 -0800

    Delete mongoDBHelper.js

--- Backend/mongoDBHelper.js
+++
@@ -1,9 +0,0 @@
-// backend/mongoDBHelper.js
-
-// Mock function to simulate saving metadata to MongoDB
-async function saveMetadataToMongoDB(metadata) {
-    // Return a mock success message for testing
-    return "Mocked response: Metadata saved to MongoDB successfully";
-}
-
-module.exports = saveMetadataToMongoDB;
